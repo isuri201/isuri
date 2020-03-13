26696940e534465997974cabb081f0991543ba06
isuri <isuripiyumali@gmail.com>
2020-03-13 12:49:31 +0530

    my comit

---
+++ src/main/java/lk/demo/isuri/IsuriApplication.java
@@ -0,0 +1,13 @@
+package lk.demo.isuri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsuriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsuriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/demo/isuri/IsuriApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.demo.isuri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsuriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
